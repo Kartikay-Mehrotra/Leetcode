cac7929ae84862e76aab252d5a69a0e000e6dc4e
Kartikay Mehrotra <74923561+Kartikay-Mehrotra@users.noreply.github.com>
2023-09-09 14:18:39 +0530

    Time: 51 ms (5.04%), Space: 120.7 MB (6.68%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+
+    int solve(int i, int n, vector<int>arr, int t, vector<vector<int>>&dp){
+        if(i>=n || t<0) return 0;
+        if(t==0) return 1;
+        if(dp[i][t]!=-1) return dp[i][t];
+        int take=solve(0, n, arr, t-arr[i], dp);
+        int ntake=solve(i+1, n, arr, t, dp);
+        return dp[i][t]=take+ntake;
+    }
+
+    int combinationSum4(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<vector<int>>dp(n+1, vector<int>(target+1, -1));
+        return solve(0, n, nums, target, dp);
+    }
+};
