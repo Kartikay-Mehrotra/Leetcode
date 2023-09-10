c45015563b995ff908d65bd0c4142363b27b5870
Kartikay Mehrotra <74923561+Kartikay-Mehrotra@users.noreply.github.com>
2023-09-10 16:46:26 +0530

    Time: 282 ms (5.38%), Space: 127.4 MB (5.00%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,vector<int>> mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]].push_back(i);
+        }
+        for(auto i:mp)
+        {
+            vector<int> t=i.second;
+            for(int j=1;j<t.size();j++)
+            {
+                if(abs(t[j]-t[j-1])<=k)
+                return true;
+            }
+        }
+        return false;
+    }
+};
