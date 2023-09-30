84f5f758834178c180b91a1c5f0a9764384a4ed3
Kartikay Mehrotra <74923561+Kartikay-Mehrotra@users.noreply.github.com>
2023-09-30 16:31:09 +0530

    Time: 78 ms (19.01%), Space: 49 MB (29.11%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.cpp
+++ 0456-132-pattern/0456-132-pattern.cpp
@@ -3,6 +3,8 @@ public:
     bool find132pattern(vector<int>& nums) {
         stack<int> s;
         int val=INT_MIN;
+        if( nums.size()<3)
+            return false;
         for(int i=nums.size()-1;i>=0 ;i--)
         {
             if(nums[i]<val)
