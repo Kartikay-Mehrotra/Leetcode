c6c94f2b993beab773441ef411f4645c174cc9ba
Kartikay Mehrotra <74923561+Kartikay-Mehrotra@users.noreply.github.com>
2023-10-02 16:07:10 +0530

    Time: 11 ms (39.33%), Space: 21.5 MB (36.69%) - LeetHub

--- 0968-binary-tree-cameras/0968-binary-tree-cameras.cpp
+++ 0968-binary-tree-cameras/0968-binary-tree-cameras.cpp
@@ -14,25 +14,28 @@ public:
     int solve(TreeNode* root,int &ans)
     {
         if(root==NULL)
+        {
             return 1;
-        
+        }
         int l=solve(root->left,ans);
         int r=solve(root->right,ans);
-        if(l==0 || r==0)
+        if(l==0|| r==0)
         {
             ans++;
             return 2;
         }
-        if(l==2 || r==2)
-        {
+        if(l==2|| r==2)
             return 1;
-        }
-        return 0;
+            
+            
+            return 0;
     }
     int minCameraCover(TreeNode* root) {
         int ans=0;
-        if(solve(root,ans)==0)
-            return ++ans;
+        int t=solve(root,ans);
+        if(t==0)
+            ans++;
+       
         return ans;
         
     }
