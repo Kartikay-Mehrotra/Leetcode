2de3858ce1cce4e81405f38aacae71a76fbfcc8d
Kartikay Mehrotra <74923561+Kartikay-Mehrotra@users.noreply.github.com>
2023-10-18 09:34:53 +0530

    Time: 247 ms (48.73%), Space: 10.9 MB (41.17%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,63 +2,19 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n,1);
-        vector<int> hash(n);
-        int last=-1;
+        vector<int> dp(n+1,1);
         int maxi=1;
         for(int i=0;i<n;i++)
         {
-            hash[i]=i;
-            for(int prev=0;prev<i;prev++)
+            for(int j=0;j<i;j++)
             {
-                if(nums[prev]<nums[i] && 1+dp[prev]> dp[i])
+                if(nums[j]<nums[i])
                 {
-                    dp[i]=1+dp[prev];
-                    hash[i]=prev;
+                    dp[i]=max(1+dp[j],dp[i]);
                 }
             }
-            if(dp[i]>maxi)
-            {
-                maxi=dp[i];
-                last=i;
-            }
-           
+            maxi=max(maxi,dp[i]);
         }
-        // vector<int> temp;
-        // temp.push_back(nums[last]);
-        // while(hash[last]!=last)
-        // {
-        //     last=hash[last];
-        //     temp.push_back(nums[last]);
-        // }
-        // reverse(temp.begin(),temp.end());
-        // for(auto i:temp)
-        // cout<<i<<" ";
-        // cout<<endl;
-        
-       
         return maxi;
     }
 };
-
-// class Solution {
-// public:
-//     int lengthOfLIS(vector<int>& nums) {
-//         int n=nums.size();
-//         vector<int> dp(n,1);
-//         for(int i=0;i<n;i++)
-//         {
-//             for(int prev=0;prev<i;prev++)
-//             {
-//                 if(nums[prev]<nums[i])
-//                 {
-//                     dp[i]=max(1+dp[prev],dp[i]);
-//                 }
-//             }
-//         }
-//         int maxi=1;
-//         for(auto i:dp)
-//         maxi=max(maxi,i);
-//         return maxi;
-//     }
-// };
