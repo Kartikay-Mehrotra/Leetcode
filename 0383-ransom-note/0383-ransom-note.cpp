17d28b3314b5388aa1cab3da98dd5b1008fa17af
Kartikay Mehrotra <74923561+Kartikay-Mehrotra@users.noreply.github.com>
2023-11-04 08:40:35 +0530

    Time: 28 ms (12.01%), Space: 9.2 MB (27.13%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+       map<char,int> mp;
+        for(auto i:magazine)
+            mp[i]++;
+        for(auto i:ransomNote)
+        {
+            if(mp.count(i))
+            {
+                mp[i]--;
+                if(mp[i]==0)
+                    mp.erase(i);
+            }
+            else
+                return false;
+        }
+        return true;
+    }
+};
