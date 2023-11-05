f3f0de884d60ee3f88940dd7efedf7f8b149b5d2
Kartikay Mehrotra <74923561+Kartikay-Mehrotra@users.noreply.github.com>
2023-11-05 22:17:51 +0530

    Time: 3 ms (41.99%), Space: 6.3 MB (49.99%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -2,20 +2,24 @@ class Solution {
 public:
     double solve(double x,long n)
     {
-        if(n==0)
+        if(n<=0)
             return 1;
-        if(n%2==0)
+        if(n&1)
         {
-            return solve(x*x,n/2);
+            return x*solve(x,n-1);
         }
-        
-        return x*solve(x,n-1);
+        else
+            return solve(x*x,n/2);
     }
     double myPow(double x, int n) {
+        if(n==1 || x==1)
+            return x;
         if(n==0)
             return 1;
-      long l=abs(n);
-        double val=solve(x,l);
-        return n>0?val:1/val;
+        int sign=n<0?-1:1;
+        long n1=abs(n);
+        double val=solve(x,n1);
+        val=sign==-1?1/val:val;
+        return val;
     }
 };
