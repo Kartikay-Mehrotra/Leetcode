fcc0db1ba1e03d297183f9cbb2794c1fe02b722d
Kartikay Mehrotra <74923561+Kartikay-Mehrotra@users.noreply.github.com>
2023-11-18 14:38:03 +0530

    Time: 59 ms (93.35%), Space: 59.3 MB (48.20%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -6,8 +6,8 @@ public:
         int ans=(j-i)*min(height[i],height[j]);
         while(i<j)
         {
-            ans=max(ans,((j-i)*min(height[i],height[j])));
-            if(height[i]<=height[j])
+            ans=max(ans,(j-i)*min(height[i],height[j]));
+            if(height[i]<height[j])
                 i++;
             else
                 j--;
